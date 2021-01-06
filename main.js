f52640d2c135b114adaf77b5ccd384d9faa1ee7f
jamilhassanuk <jamil@jamilhassan.co.uk.com>
2021-01-06 18:08:02 +0000

    more formatting on the output 2

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ const quoteGame = () => {
     quote1 = philosopherQuote[randomNum()];
     quote2 = technologyQuote[randomNum()];
     quote3 = funQuote[randomNum()];
-    console.log('FAMOUS QUOTES MACHINE !!! \n\n');
+    console.log('\n\nFAMOUS QUOTES MACHINE !!! \n\n');
     console.log('========== Philosopher quote ==========\n' + 'Author: ' + quote1.author + '\n' + 'Quote: ' + quote1.quote + '\n');
     console.log('========== Technology quote  ==========\n' + 'Author: ' + quote2.author + '\n' + 'Quote: ' + quote2.quote + '\n');
     console.log('========== Fun quote  =================\n' + 'Author: ' + quote3.author + '\n' + 'Quote: ' + quote3.quote);
